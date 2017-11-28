d582179737f03f2ce47ae009f19f6fb5220d0acc
Tomas Trebicky <tomas.trebicky@nerdsonsite.com>
2017-11-28 16:01:31 -0500

    Removed stuff that never should have been there.

--- bash/custom_configs/aliases.sh
+++ bash/custom_configs/aliases.sh
@@ -1,11 +1,3 @@
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
-
-[ -d "/opt/brcm/hndtools-mipsel-linux/bin" ] && path="${path}:/opt/brcm/hndtools-mipsel-linux/bin"
-[ -d "/opt/brcm/hndtools-mipsel-uclibc/bin" ] && path="${path}:/opt/brcm/hndtools-mipsel-uclibc/bin"
-[ -d "/opt/brcm-arm/bin" ] && path="${path}:/opt/brcm-arm/bin"
-if [ -n "${path}" ]; then
-  alias asus="echo export PATH=${PATH}:${path}"
-  unset path
-fi
